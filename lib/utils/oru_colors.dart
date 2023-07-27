99369e0e1df684414c74bf77515ef1795c51c7d7
Sandy <sandeepshah103@gmail.com>
2023-07-27 17:09:54 +0530

    [200~added release apk to root directory

--- lib/utils/oru_colors.dart
+++ lib/utils/oru_colors.dart
@@ -1,27 +1,9 @@
 import 'dart:ui';
 
 class OruColors {
-  static const neutralMain700 = Color(0xFF1A1A1A);
-
-  static const neutralMain500 = Color(0xFF666666);
-
-  static const neutralMain400 = Color(0xFF333333);
-
-  static const neutralMain300 = Color(0xFFEBEBEB);
-
   static const defaultTextColor = Color(0xFF414141);
 
   static const backgroundColor = Color(0xFFF5F7F9);
-
-  static const inputBox = Color(0xFFF2F2F2);
-
-  static const primaryMain500 = Color(0xff192C6A);
-
-  static const primaryMain200 = Color(0xffD8F0C8);
-
-  static const primaryMain400 = Color(0xff1B97C9);
-
-  static const primaryMain300 = Color(0xff8C94AA);
 
   static const red = Color.fromRGBO(235, 87, 87, 1);
 
@@ -29,12 +11,7 @@ class OruColors {
 
   static const white = Color(0xffffffff);
 
-  static const grey = Color(0xfff5f7f9);
-
   static const yellow = Color(0xffF9C414);
-  static const gainsboro = Color(0xFFCCCCCC);
-  static const kGreenBg = Color(0xFF6CC52B);
-  static const lightBlue = Color(0xFFBEF1FF);
 
   static const appBar = Color(0xFF2C2E43);
   static const sellGradient2 = Color(0xFF595E72);
